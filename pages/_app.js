d997ef754507ec79cd661b3b16669003ba08f9a9
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-08 23:35:30 +0100

    169 useContext(NotificationContext);

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-
-import Notification from "../components/ui/notification";
 import { NotificationContextProvider } from "../store/notification-context";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
@@ -8,6 +6,7 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <NotificationContextProvider>
+      {/* Layout to idelany komponent do renderowania Notifikacji, samo jego imię "layout" z definicji to wrapper dla wizalnego kontentu*/}
       <Layout>
         <Head>
           <title>Next Events</title>
@@ -18,7 +17,6 @@ function MyApp({ Component, pageProps }) {
           />
         </Head>
         <Component {...pageProps} />
-        <Notification title="Test" message="This is a test" status="error" />
       </Layout>
     </NotificationContextProvider>
   );
