4973d7c2527ee0d817d064f3d197a671432998b5
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-08 20:59:35 +0100

    156 połaczenie z bazą mongodb haslo usuniete

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,27 +1,34 @@
 // ZAWSZE ustawiać res dla NIeudanych i Udanych akcji
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
-    if(req.method === 'POST'){
-        // zapisywanie do newslettera, POST ma req.body a GET NIE
-        const userEmail = req.body.email;
+async function handler(req, res) {
+  //handlery mogą być asynchroniczne a troche latwiej potem z mongodb pracowac
+  if (req.method === "POST") {
+    // zapisywanie do newslettera, POST ma req.body a GET NIE
+    const userEmail = req.body.email;
 
-        // prosta walidacja adresu email
-        if(!userEmail || !userEmail.includes('@')){
-            // lpeiej zrobić walidację na backendzie niż na froncie, bo tam, też może user grzebac w kodzie klienta
-            // HTTP status 442 - dane od usera są złe
-            res.status(422).json({message: 'Invalid email address.'});
-            return; //cancel function execution 
-        }
-
-        console.log(userEmail);
-        // HTTP 201 success resource was added
-        res.status(201).json({message: 'Signed up'});
-
-    }else{
-        res.status(200).json({message: 'GET method'});
-
+    // prosta walidacja adresu email
+    if (!userEmail || !userEmail.includes("@")) {
+      // lpeiej zrobić walidację na backendzie niż na froncie, bo tam, też może user grzebac w kodzie klienta
+      // HTTP status 442 - dane od usera są złe
+      res.status(422).json({ message: "Invalid email address." });
+      return; //cancel function execution
     }
 
+    const client = await MongoClient.connect(
+      "url do mobngodb"
+    );
+    const db = client.db(); //nie trzeb apodawać nazwy bazy bo jest już ona w connecting string
+
+    await db.collection("emails").insertOne({ email: userEmail }); //zwraca promisa
+    client.close();
+
+    console.log(userEmail);
+    // HTTP 201 success resource was added
+    res.status(201).json({ message: "Signed up" });
+  } else {
+    res.status(200).json({ message: "GET method" });
+  }
 }
 
 export default handler;
