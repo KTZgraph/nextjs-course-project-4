f555a8a2a83c7c398807744399c7e347a3bd8a61
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-09 00:04:47 +0100

    sa wszsytkie pliki by dodac cale notification.js

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+
 import classes from './notification.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function Notification(props) {
+  const notificationCtx = useContext(NotificationContext);
 
   const { title, message, status } = props;
 
@@ -21,8 +25,7 @@ function Notification(props) {
   const activeClasses = `${classes.notification} ${statusClasses}`;
 
   return (
-    // <div className={activeClasses} onClick={notificationCtx.hideNotification}>
-    <div className={activeClasses}>
+    <div className={activeClasses} onClick={notificationCtx.hideNotification}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
