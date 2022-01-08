9178769b89d183c6896fc77f95920ff0cad1982a
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-08 19:46:33 +0100

    153 API dodawania + wyswietlania komentarzy

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,43 @@
+// /api/comments/some-event-id wiecej sensu w takiej ścieżce
+function handler(req, res) {
+  //trzeba wiedzieć dla którego eventu dodaję/pobieram komentarze
+
+  const eventId = req.query.eventId; //eventId z nazwy pliku [eventId].js
+  if (req.method === "POST") {
+    //add serwer-side validation, NIE można UFAĆ walidacji po stronie klienta!
+    const { email, name, text } = req.body;
+
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input" });
+      return;
+    }
+    const newComment = {
+      id: new Date().toISOString(), //losowe id na potrzeby develompemntu
+      email,
+      name,
+      text,
+    };
+
+    console.log(newComment);
+    res.status(201).json({ message: "Added comment.", comment: newComment });
+  }
+
+  if (req.method === "GET") {
+    //   lista komentarzy
+    const dummyList = [
+      { id: "c1", name: "Max", text: "A first comment" },
+      { id: "c2", name: "Manuel", text: "A second comment" },
+    ];
+    res.status(201).json({comments: dummyList});
+  }
+
+  //dla innych metod nie ma zwrotki, bo nie planuję obsługi innych metod
+}
+
+export default handler;
