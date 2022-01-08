17cd5732117ff2b7771c7f1ef9c55cab69e3f3cd
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-08 23:35:16 +0100

    169 useContext(NotificationContext);

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,26 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from "react"; // trzeba hooka useContext bo w komponencie Layout we can't tap our context
 
-import MainHeader from './main-header';
+import MainHeader from "./main-header";
+import Notification from "../ui/notification";
+import NotificationContext from "../../store/notification-context"; //żeby połaczyć się z obiektem contextu
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {/* renderowanie warunkowe */}
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   );
 }
